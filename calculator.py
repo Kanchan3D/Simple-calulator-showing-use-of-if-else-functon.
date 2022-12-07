0a6292077ab26c313b11483c644f87fdd7e314d0
Kanchan Dasila <118431103+Kanchan3D@users.noreply.github.com>
2022-12-07 18:34:53 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,17 @@
+a=int(input())
+if(a==1):
+    n1=int(input("enter 1st number to be added:"))
+    n2=int(input("enter 2nd number to be added:"))
+    print("the sum is=",n1+n2)
+elif(a==2):
+    n1 = int(input("enter 1st number to be subtrated:"))
+    n2 = int(input("enter 2nd number to be subtrated:"))
+    print("the difference is=", n1-n2)
+elif(a==3):
+    n1 = int(input("enter 1st number to be multipied:"))
+    n2 = int(input("enter 2nd number to be multiplied:"))
+    print("the product is=", n1*n2)
+elif(a==4):
+    n1 = int(input("enter 1st number to be divided:"))
+    n2 = int(input("enter 2nd number to divide with:"))
+    print("the divided value is=", n1/n2)
